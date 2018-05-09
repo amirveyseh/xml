2022f9b41a9936bc978fef42ce4cc164a4efd1b1
Amir Veyseh <Amirveyseh@gmail.com>
2018-05-09 21:10:14 +0430

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 http
   .createServer((req, res) => {
     // Create TwiML response
@@ -11,6 +14,6 @@ http
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
   })
-  .listen(1337, '127.0.0.1');
+  .listen(server_port, server_ip_address);
 
-console.log('TwiML server running at http://127.0.0.1:1337/');
+console.log('TwiML server running at http://'+server_ip_address+':'+server_port+'/');
